30a24383dc2663102138289a0b60b259e1412feb
Vivek Kumar <v.kumar533359@gmail.com>
2023-01-29 15:20:38 +0530

    rectangle program

---
+++ solid_rectangle.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    //declare number
+    int n;
+
+    //print this statement
+    cout<<"Enter how many rows you want to insert:";
+    //Take input from user
+    cin>>n;
+
+    // condition for outer row
+    for(int row=0;row<n;row++){
+
+        //condition for inner row
+        for(int col=0;col<n;col++){
+
+            //print star
+            cout<<"*  ";
+        }
+        // for next line
+        cout<<endl;
+    }
+
+}
